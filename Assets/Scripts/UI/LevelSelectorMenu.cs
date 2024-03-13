2d9e1186f0cf55c98eb5d5c17068c3438bcfa947
Jubby liang <liangzhengyebjtt@gmail.com>
2024-03-13 01:12:23 -0400

    Final commit(submission time)

--- Assets/Scripts/UI/LevelSelectorMenu.cs
+++ Assets/Scripts/UI/LevelSelectorMenu.cs
@@ -4,17 +4,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Rola.UI
 {
     public sealed class LevelSelectorMenu : MonoBehaviour
     {
+        [SerializeField] private RawImage _image;
+
+        private void OnEnable() => StartCoroutine(DoThing());
+
         public void OnLevelSelected(int level)
         {
             GameManager.Instance.LoadLevel(level);
 
             UIManager.Instance.DisableAll();
             UIManager.DoButtonSound();
+        }
+
+        private IEnumerator DoThing()
+        {
+            while(gameObject.activeInHierarchy)
+            {
+                yield return new WaitForSeconds(2f);
+
+                if(_image.color.a < 0.005)
+                    yield break;
+
+                _image.color -= new Color(0f, 0f, 0f, 0.004f);
+                _image.rectTransform.localPosition += new Vector3(0f, 0.05f, 0f);
+            }
         }
     }
 }
